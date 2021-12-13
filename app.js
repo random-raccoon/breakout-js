eeb1ada6175e0dc48c8ad48fc31d1a3d0fa05a06
random-raccoon <rnd.raccoon@gmail.com>
2021-12-12 19:32:55 -0500

    Bounce ball off walls

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+const ballRadius = 10;
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
@@ -8,7 +9,7 @@ let dy = -2;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -18,7 +19,14 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawBall();
-    
+
+    // TODO: Improve bouncing.
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
     x += dx;
     y += dy;
 }
